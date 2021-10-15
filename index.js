bd973730365a962e9f246bc914067d640b321d31
Ron <rongrammer@hotmail.com>
2021-10-15 12:16:12 +0800

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-// abstract holds the db instance
-// contains the save and remove function
-// 
-const Mongodeler = require('./Mongodeler');
-let instance = new Mongodeler();
-module.exports = instance;
